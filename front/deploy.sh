a743b40515e687ae3e165f1c0a7e0eeab08d6678
Lyes Harrar <lyesharrar@gmail.com>
2021-11-14 23:01:25 +0100

    Update deploy.sh

--- front/deploy.sh
+++ front/deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:pauladeville/groupomania.git master:gh-pages
+git push -f git@https://github.com/LyesHarrar/LyesHarrar_7_13112021.git master:gh-pages
 
 cd -
